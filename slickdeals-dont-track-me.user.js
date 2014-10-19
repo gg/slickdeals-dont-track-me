979cfa657490ce3aef87b65860ceea477eaf2bc3
Gregg Gajic <gregggajic@gmail.com>
2014-10-19 15:56:00 -0500

    Create slickdeals-dont-track-me.user.js

---
+++ slickdeals-dont-track-me.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name       Slickdeals Don't Track Me!
+// @version    1.0
+// @description  Replaces outgoing Slickdeals tracking links with direct links.
+// @match      http://slickdeals.net/f/*
+// @namespace https://github.com/gg/slickdeals-dont-track-me
+// @author    Gregg Gajic <https://github.com/gg>
+// @license    MIT
+// ==/UserScript==
+
+(function() {
+  var getQueryStringParameters = function (url) {
+    // See: http://stackoverflow.com/a/3855394
+    return (function(a) {
+        if (a == "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; ++i)
+        {
+            var p=a[i].split('=', 2);
+            if (p.length == 1)
+                b[p[0]] = "";
+            else
+                b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })((url.split('?')[1] || '').split('&'));
+  };
+
+  var aElements = document.getElementById('maincontent').getElementsByTagName('a');
+  for (var i = 0, a; a = aElements[i]; i++) {
+    var queryParamters = getQueryStringParameters(a.href);
+    var directUrl = queryParamters['u2'];
+    if (directUrl) {
+      a.href = directUrl;
+      a.onclick = '';
+    };
+  }
+})();
